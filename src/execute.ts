028c46ff7b7cc34740de8c6c6b75c6c63238c811
07akioni <07akioni2@gmail.com>
2023-01-17 03:12:43 +0800

    exit with code 1 if there's error

--- src/execute.ts
+++ src/execute.ts
@@ -10,46 +10,59 @@ import { RuleName } from './rules/types'
 const statAsync = promisify(fs.stat)
 
 export async function execute(config: Config) {
-  for (const override of config.overrides) {
-    const files = Array.isArray(override.files)
-      ? override.files
-      : [override.files]
-    for (const fileGlob of files) {
-      const deferred = defer()
-      glob(
-        fileGlob,
-        { ignore: ['node_modules/**/*'] },
-        async (error, filesOrDirs) => {
-          if (error) {
-            console.error(error)
-            deferred.reject(
-              new Error(formatMessage('execute', 'internal error: glob error'))
-            )
-            return
-          }
-          const files: string[] = []
-          const dirs: string[] = []
-          for (const fileOrDir of filesOrDirs) {
-            if ((await statAsync(fileOrDir)).isDirectory()) {
-              dirs.push(fileOrDir)
-            } else {
-              files.push(fileOrDir)
+  let shouldExitNormally = true
+  try {
+    for (const override of config.overrides) {
+      const files = Array.isArray(override.files)
+        ? override.files
+        : [override.files]
+      for (const fileGlob of files) {
+        const deferred = defer()
+        glob(
+          fileGlob,
+          { ignore: ['node_modules/**/*'] },
+          async (error, filesOrDirs) => {
+            if (error) {
+              console.error(error)
+              deferred.reject(
+                new Error(
+                  formatMessage('execute', 'internal error: glob error')
+                )
+              )
+              return
             }
+            const files: string[] = []
+            const dirs: string[] = []
+            for (const fileOrDir of filesOrDirs) {
+              if ((await statAsync(fileOrDir)).isDirectory()) {
+                dirs.push(fileOrDir)
+              } else {
+                files.push(fileOrDir)
+              }
+            }
+            const { rules } = override
+            for (const ruleName in rules) {
+              const ruleExecutor = ruleExecutors[ruleName as RuleName]
+              const { messages } = await ruleExecutor({
+                files,
+                dirs,
+                params: rules[ruleName as RuleName] as any
+              })
+              if (shouldExitNormally && messages.length) {
+                shouldExitNormally = false
+              }
+              printDiagnostics(ruleName, messages)
+            }
+            deferred.resolve()
           }
-          const { rules } = override
-          for (const ruleName in rules) {
-            const ruleExecutor = ruleExecutors[ruleName as RuleName]
-            const { messages } = await ruleExecutor({
-              files,
-              dirs,
-              params: rules[ruleName as RuleName] as any
-            })
-            printDiagnostics(ruleName, messages)
-          }
-          deferred.resolve()
-        }
-      )
-      await deferred.promise
+        )
+        await deferred.promise
+      }
     }
+  } catch (error) {
+    console.error('internal error', error)
+  }
+  if (!shouldExitNormally) {
+    process.exit(1)
   }
 }
